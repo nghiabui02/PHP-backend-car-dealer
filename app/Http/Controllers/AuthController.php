fcdd63746ae9f22b30c13f7d208d935cc0927c33
nghiabui02 <buihuunghiabg@gmail.com>
2024-10-25 00:34:09 +0700

    update register and login api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,29 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Validation\ValidationException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request): JsonResponse
     {
-        $attributes = $request->validate([
+        $data = $request->all();
+        \Log::info($data);
+        $attributes = Validator::make($data,[
             'login' => ['required', 'string'],
             'password' => ['required', 'string'],
         ]);
 
-        if (filter_var($attributes['login'], FILTER_VALIDATE_EMAIL)) {
+        if ($attributes->fails()) {
+            return response()->json($attributes->messages(), 400);
+        }
+
+        $dataSend = $attributes->validated();
+
+        if (filter_var($dataSend['login'], FILTER_VALIDATE_EMAIL)) {
             $loginType = 'email';
-        } elseif (preg_match('/^[0-9]{10,15}$/', $attributes['login'])) {
+        } elseif (preg_match('/^[0-9]{10,15}$/', $dataSend['login'])) {
             $loginType = 'phone_number';
         } else {
             $loginType = 'username';
         }
 
         $credentials = [
-            $loginType => $attributes['login'],
-            'password' => $attributes['password'],
+            $loginType => $dataSend['login'],
+            'password' => $dataSend['password'],
         ];
 
         try {
@@ -41,17 +51,26 @@ class AuthController extends Controller
         return response()->json(['user' => $user ,'access_token' => $token, 'token_type' => 'Bearer']);
     }
 
+    /**
+     * @throws ValidationException
+     */
     public function register(Request $request): JsonResponse
     {
-        $attributes = $request->validate([
+        $data = $request->all();
+        \Log::info($data);
+        $attributes = Validator::make($data, [
             'email' => ['required', 'email', 'unique:users,email'],
             'username' => ['required', 'string', 'unique:users,username', 'min:5', 'max:8'],
             'password' => ['required', 'string', 'max:10', 'min:6', 'confirmed'],
             'name' => ['required', 'string', 'max:10'],
             'phone_number' => ['required', 'string'],
         ]);
+        if ($attributes->fails()) {
+            return response()->json($attributes->messages(), 400);
+        }
+        $dataRegister = $attributes->validated();
 
-        User::register($attributes);
+        User::register($dataRegister);
 
         return response()->json(['message' => 'User registered successfully', 'user' => $attributes], 201);
     }
